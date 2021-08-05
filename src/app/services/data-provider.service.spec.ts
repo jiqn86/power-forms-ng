c074ff6c0dce829a770df8911ecf2a4f1b81cc4e
jiqn86 <jiqn86@gmail.com>
2021-08-05 10:50:20 -0400

    Update data-provider.service.spec.ts
    services DataProviderService

--- src/app/services/data-provider.service.spec.ts
+++ src/app/services/data-provider.service.spec.ts
@@ -1,26 +1,25 @@
-import { HttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 
 import { DataProviderService } from './data-provider.service';
 
-class HttpClientMock {
-  get = jasmine.createSpy('httpClient.get');
-}
 
 describe('DataProviderService', () => {
   let service: DataProviderService;
+  let httpTestingController: HttpClientTestingModule;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [DataProviderService,{
-        provide:HttpClient,
-        useClass: HttpClientMock
-      }]
+      imports: [HttpClientTestingModule],
+      providers: [DataProviderService]
     });
     service = TestBed.inject(DataProviderService);
+    httpTestingController = TestBed.inject(HttpClientTestingModule)
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+    expect(service.getInfo()).toBeTruthy();
   });
+
 });
